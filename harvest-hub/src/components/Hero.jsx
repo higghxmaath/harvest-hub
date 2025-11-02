0ff5dcec415fb65cc5d7f5cf7794cfc5eb68f2ce
higghxmaath <ayanmattex@gmail.com>
2025-11-02 01:52:36 +0100

    Update Hero.jsx

--- harvest-hub/src/components/Hero.jsx
+++ harvest-hub/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero() {
   return (
     <div  className="hero">
-     <h2 className='heroH2'>Harvest Hub</h2>
+     <h2 className='heroH2'>Kiosk Daily Groceries</h2>
      <h1>Local Market Catalog</h1>
      <p>Shop fresh from local vendors</p>
     </div>
